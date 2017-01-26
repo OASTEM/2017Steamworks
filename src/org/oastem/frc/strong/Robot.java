82587761ae7ad5304e4f8546652b90d5d06e1d2d
yveys210 <yveys210@gmail.com>
2017-01-25 16:41:34 -0800

    Update self-correcting autonomous

--- src/org/oastem/frc/strong/Robot.java
+++ src/org/oastem/frc/strong/Robot.java
@@ -7,36 +7,20 @@ import org.oastem.frc.control.TalonDriveSystem;
 import org.oastem.frc.sensor.LVMaxSonarEZUltrasonic;
 
 import edu.wpi.cscore.AxisCamera;
+import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * This is a demo program showing the use of the RobotDrive class. The
- * SampleRobot class is the base of a robot application that will automatically
- * call your Autonomous and OperatorControl methods at the right time as
- * controlled cnby the switches on the driver station or the field controls.
- *
- * The VM is configured to automatically run this class, and to call the
- * functions . If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- *
- * WARNING: While it may look like a good choice to use for your code if you're
- * inexperienced, don't. Unless you know what you are doing, complex code will
- * be much more difficult under this system. Use IterativeRobot or Command-Based
- * instead if you're new.
- */
 public class Robot extends SampleRobot {
 	// Ports
 	private final int FRONT_LEFT_CAN_DRIVE = 0;
 	private final int FRONT_RIGHT_CAN_DRIVE = 2;
 	private final int BACK_LEFT_CAN_DRIVE = 1;
 	private final int BACK_RIGHT_CAN_DRIVE = 3;
-	
-	private final int INPUT_PORT = 0;
+	private final int SONIC_SENSOR_INPUT_PORT = 0;
 
 	// Values
 	private final int DRIVE_ENC_CODE_PER_REV = 2048;
@@ -48,8 +32,8 @@ public class Robot extends SampleRobot {
 	private LogitechGamingPad padSupport;
 	private SmartDashboard dash;
 	private CameraServer server;
-	//private UsbCamera camera;
-	//private UsbCamera camera2;
+	private UsbCamera camera;
+	private UsbCamera camera2;
 	private AxisCamera visionCamera; 
 	private LVMaxSonarEZUltrasonic sonicSensor;
 	private Timer timer; 
@@ -80,20 +64,20 @@ public class Robot extends SampleRobot {
 		pad = new LogitechGamingPad(0);
 		padSupport = new LogitechGamingPad(1);
 
-		//server = CameraServer.getInstance();
-		//camera = new UsbCamera("camera1", 1);
-		//camera2 = new UsbCamera("camera2", 0);
-		//visionCamera = new AxisCamera("visionCamera", "10.40.79.88");
+		//camera objects 
+		server = CameraServer.getInstance();
+		camera = new UsbCamera("camera1", 1);
+		camera2 = new UsbCamera("camera2", 0);
+		visionCamera = new AxisCamera("visionCamera", "10.40.79.88");
+		camera.setResolution(160, 120);
+		camera2.setResolution(160, 120);
+		visionCamera.setResolution(480, 360);
+		server.startAutomaticCapture(camera);
+		server.startAutomaticCapture(camera2);
+		server.startAutomaticCapture(visionCamera);
 		
-		//camera.setResolution(160, 120);
-		//camera2.setResolution(160, 120);
-		//visionCamera.setResolution(480, 360);
+		sonicSensor = new LVMaxSonarEZUltrasonic(SONIC_SENSOR_INPUT_PORT);
 		
-		//server.startAutomaticCapture(camera);
-		//server.startAutomaticCapture(camera2);
-		//server.startAutomaticCapture(visionCamera);
-		
-		sonicSensor = new LVMaxSonarEZUltrasonic(INPUT_PORT);
 		timer = new Timer();
 		
 		table = NetworkTable.getTable("GRIP/myContoursReport");
@@ -102,21 +86,9 @@ public class Robot extends SampleRobot {
 
 	}
 
-	/*
-	public void robotInit() {
-		dash = new SmartDashboard();
-		talonDrive.calibrateGyro();
-
-		pad = new LogitechGamingPad(0);
-		padSupport = new LogitechGamingPad(1);
-
-		camera = CameraServer.getInstance();
-		camera.startAutomaticCapture();
-	}*/
-
 	public void autonomous() {
 		int caseNumber = 0;
-		int caseNumber2 = 1;
+		int caseNumber2 = 5;
 		//if vision target is centered, but ultrasonic sensor reads too high a value --> case 1
 			//case 1: move forwards until ultrasonic sensor reads the correct value
 			//caseNumber = 1;
@@ -184,29 +156,44 @@ public class Robot extends SampleRobot {
 						caseNumber2 = 2;
 					break;
 				case 2:
-					if (timer.get() < 5)
+					if (timer.get() < 4)
 						talonDrive.tankDrive(0, 0);
 					else 
 						caseNumber2 = 3;
 					break;
 				case 3: //not centered 
 					talonDrive.tankDrive(0.7, -0.7);
-					if (centerY.length == 2 && centerX.length == 2)
+					if (/*talonDrive.getAngle() < 65 && talonDrive.getAngle() > 55 && */centerY.length == 2 && centerX.length == 2)
 						caseNumber2 = 4; 
 					break;
 				case 4: //centered but not close enough to target 
 					centerXCoor = (centerX[0] + centerX[1])/2;
-					if (centerXCoor >= 300 && centerXCoor <= 340)
+					if (centerXCoor >= 220 && centerXCoor <= 260)
 					{
 						talonDrive.tankDrive(0, 0);
 						caseNumber2 = 5; 
 					}
 					break; 
 				case 5: //centered and close to target 
-					if (sonicSensor.getDistance() > 8 && sonicSensor.getDistance() < 10)
-						talonDrive.tankDrive(0, 0);
+					if (centerY.length == 2 && centerX.length == 2)
+						centerXCoor = (centerX[0] + centerX[1])/2;
+					if (!(sonicSensor.getDistance() > 14 && sonicSensor.getDistance() < 16))
+					{
+						if (centerXCoor >= 220 && centerXCoor <= 260)
+							talonDrive.tankDrive(-0.2, -0.2);
+						else if (centerXCoor <= 220)
+							talonDrive.tankDrive(-0.3, -0.0);
+						else if (centerXCoor >= 260)
+							talonDrive.tankDrive(-0.0, -0.3);	
+						else if (centerY.length < 2 && centerX.length < 2)
+							talonDrive.tankDrive(-0.2, -0.2);
+					}
 					else
-						talonDrive.tankDrive(-0.2, -0.2);
+							caseNumber2 = 6;
+					break;
+				case 6:
+					if (sonicSensor.getDistance() > 14 && sonicSensor.getDistance() < 16)
+						talonDrive.tankDrive(0, 0);
 					break;
 			}
 			SmartDashboard.putNumber("Ultrasonic reading: ", sonicSensor.getDistance());
@@ -232,6 +219,8 @@ public class Robot extends SampleRobot {
 			
 			SmartDashboard.putNumber("Ultrasonic reading: ", sonicSensor.getDistance());
 			SmartDashboard.putNumber("Ultrasonic raw reading; ", sonicSensor.getVoltage());
+			
+			SmartDashboard.putNumber("Gyroscope value: ", talonDrive.getAngle());
 		}
 	}
 
