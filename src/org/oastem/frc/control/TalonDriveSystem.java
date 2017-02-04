d78f5c90d3925204e91f894037d93fef94c7eaf7
yveys210 <yveys210@gmail.com>
2017-02-04 14:05:52 -0800

    Complete button mapping

--- src/org/oastem/frc/control/TalonDriveSystem.java
+++ src/org/oastem/frc/control/TalonDriveSystem.java
@@ -56,6 +56,8 @@ public class TalonDriveSystem {// (:
 		backRightDrive = new CANTalon(rightRear);
 		backLeftDrive = new CANTalon(leftRear);
 		
+		
+		
 		frontRightDrive.enableBrakeMode(true);
 		frontLeftDrive.enableBrakeMode(true);
 		backRightDrive.enableBrakeMode(true);
--- src/org/oastem/frc/strong/Robot.java
+++ src/org/oastem/frc/strong/Robot.java
@@ -1,5 +1,6 @@
 package org.oastem.frc.strong;
 
+import org.oastem.frc.C;
 import org.oastem.frc.LogitechGamingPad;
 import org.oastem.frc.control.TalonDriveSystem;
 import org.oastem.frc.sensor.LVMaxSonarEZUltrasonic;
@@ -9,200 +10,215 @@ import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends SampleRobot {
-	// Ports
-	private final int FRONT_LEFT_CAN_DRIVE = 0;
-	private final int FRONT_RIGHT_CAN_DRIVE = 2;
-	private final int BACK_LEFT_CAN_DRIVE = 1;
-	private final int BACK_RIGHT_CAN_DRIVE = 3;
-	private final int SONIC_SENSOR_INPUT_PORT = 0;
-
-
-	// Values
-	private final int DRIVE_ENC_CODE_PER_REV = 2048;
-	private final int DRIVE_WHEEL_DIAM = 8;
-	private final int CAMERA_RESOLUTION_WIDTH = 480;
-	private final int CAMERA_RESOLUTION_HEIGHT = 360; 
-	
 	// Objects
 	private TalonDriveSystem talonDrive = TalonDriveSystem.getInstance();
+	private Talon conveyorMotor;
+	private Talon winchMotor;
 	private LogitechGamingPad pad;
-	private LogitechGamingPad padSupport;
 	private CameraServer server;
 	private UsbCamera camera;
 	private UsbCamera camera2;
-	private AxisCamera visionCamera; 
+	private AxisCamera visionCamera;
 	private LVMaxSonarEZUltrasonic sonicSensor;
-	private Timer timer; 
+	private Timer timer;
 	private PowerDistributionPanel pdp;
 
 	// Joystick commands
 	private double slowTrigger;
 	private boolean eStop1Pressed;
-	private boolean eStop2Pressed;
-
-	//Network Table
-	private NetworkTable table;
+	private boolean reverseDirectionPressed;
+	private boolean reverseDirectionToggle;
+	private boolean sprintLeft;
+	private boolean sprintRight;
+	private boolean conveyorPressed;
+	private boolean conveyorToggle;
+	private double winch;
 	
-	//Camera Stuff
+	// Network Table
+	private NetworkTable table;
+
+	// Camera Stuff
 	private double[] defaultValue = new double[0];
-	private double[] centerY; 
+	private double[] centerY;
 	private double[] centerX;
 	private double centerXCoor = 0;
-	private double cameraWidth = 160;
-	private double cameraLength = 120; 
 	
-	//Autonomous Cases
-	private final int DRIVE_STRAIGHT = 1;
-	private final int MOMENTUM_STOP= 2;
-	private final int TURN = 3; 
-	private final int STOP_WHEN_CENTERED = 4; 
-	private final int SELF_CORRECT_DRIVE = 5; 
-	private final int STOP_WHEN_CLOSE_ENOUGH = 6; 
-	
-	//Autonomous Values
-	private final int HALF_RES_LEFT = CAMERA_RESOLUTION_WIDTH/2 - 20;
-	private final int HALF_RES_RIGHT = CAMERA_RESOLUTION_WIDTH/2 + 20;
-	private final double SWERVE_DRIVE_SPEED = 0.3;
-	private final double STRAIGHT_DRIVE_SPEED = 0.3;
-	private final double SLOWER_DRIVE_SPEED = 0.2;
-	private final double TURN_SPEED = 0.8;
-	private final double STOP_SPEED = 0;
-	private final int MIN_DISTANCE = 14;
-	private final int MAX_DISTANCE = 16; 
-	
-
 	public Robot() {
-		talonDrive.initializeTalonDrive(FRONT_LEFT_CAN_DRIVE, BACK_LEFT_CAN_DRIVE, FRONT_RIGHT_CAN_DRIVE,
-				BACK_RIGHT_CAN_DRIVE, DRIVE_ENC_CODE_PER_REV, DRIVE_WHEEL_DIAM);
+		talonDrive.initializeTalonDrive(C.Port.FRONT_LEFT_CAN_DRIVE, C.Port.BACK_LEFT_CAN_DRIVE,
+				C.Port.FRONT_RIGHT_CAN_DRIVE, C.Port.BACK_RIGHT_CAN_DRIVE, C.Drive.DRIVE_ENC_CODE_PER_REV,
+				C.Drive.DRIVE_WHEEL_DIAM);
 		talonDrive.calibrateGyro();
 
 		pad = new LogitechGamingPad(0);
-		padSupport = new LogitechGamingPad(1);
 
-		//camera objects 
+		// Camera Objects
 		server = CameraServer.getInstance();
-		//camera = new UsbCamera("camera1", 1);
-		//camera2 = new UsbCamera("camera2", 0);
+		camera = new UsbCamera("camera", 0);
+		camera2 = new UsbCamera("camera2", 1);
 		visionCamera = new AxisCamera("visionCamera", "10.40.79.88");
-		//camera.setResolution(160, 120);
-		//camera2.setResolution(160, 120);
+		camera.setResolution(160, 120);
+		camera2.setResolution(160, 120);
 		visionCamera.setResolution(480, 360);
-		//server.startAutomaticCapture(camera);
-		//server.startAutomaticCapture(camera2);
+		server.startAutomaticCapture(camera);
+		server.startAutomaticCapture(camera2);
 		server.startAutomaticCapture(visionCamera);
-		
-		sonicSensor = new LVMaxSonarEZUltrasonic(SONIC_SENSOR_INPUT_PORT);
-		
+
+		sonicSensor = new LVMaxSonarEZUltrasonic(C.Port.SONIC_SENSOR_INPUT_PORT);
+
 		timer = new Timer();
-		
+
 		table = NetworkTable.getTable("GRIP/myContoursReport");
 		centerY = table.getNumberArray("centerY", defaultValue);
-		centerX  = table.getNumberArray("centerX", defaultValue);
-		
+		centerX = table.getNumberArray("centerX", defaultValue);
+
 		pdp = new PowerDistributionPanel();
 		pdp.clearStickyFaults();
+
+		reverseDirectionToggle = false;
+		conveyorToggle = false;
+
 	}
 
-	
 	public void autonomous() {
 		timer.start();
-		int autonomousCase = SELF_CORRECT_DRIVE;
-		
+		int autonomousCase = C.Auto.SELF_CORRECT_DRIVE;
+
 		while (isAutonomous() && isEnabled()) {
 			centerY = table.getNumberArray("centerY", defaultValue);
 			centerX = table.getNumberArray("centerX", defaultValue);
-			
-			switch (autonomousCase)
-			{
-				case DRIVE_STRAIGHT:
-					if (timer.get() < 1.3)
-						talonDrive.reverseTankDrive(STRAIGHT_DRIVE_SPEED, STRAIGHT_DRIVE_SPEED);
-					else 
-						autonomousCase = MOMENTUM_STOP;
-					break;
-					
-				case MOMENTUM_STOP:
-					if (timer.get() < 4)
-						talonDrive.tankDrive(STOP_SPEED, STOP_SPEED);
-					else 
-						autonomousCase = TURN;
-					break;
-					
-				case TURN: 
-					talonDrive.tankDrive(-TURN_SPEED, TURN_SPEED);
-					
-					if (/*talonDrive.getAngle() < 65 && talonDrive.getAngle() > 55  && */ centerY.length == 2 && centerX.length == 2)
-					{
-						autonomousCase = STOP_WHEN_CENTERED; 
-					}
-					break;
-					
-				case STOP_WHEN_CENTERED: 
-					centerXCoor = (centerX[0] + centerX[1])/2;
-					if (centerXCoor >= HALF_RES_LEFT && centerXCoor <= HALF_RES_RIGHT)
-					{
-						talonDrive.tankDrive(STOP_SPEED, STOP_SPEED);
-						autonomousCase = SELF_CORRECT_DRIVE; 
-					}
-					break; 
-					
-				case SELF_CORRECT_DRIVE: 
-					if (centerY.length == 2 && centerX.length == 2)
-						centerXCoor = (centerX[0] + centerX[1])/2;
-					SmartDashboard.putString("reached: ", "true");
-					if (!(sonicSensor.getDistance() > MIN_DISTANCE && sonicSensor.getDistance() < MAX_DISTANCE))
-					{
-						if (centerXCoor >= HALF_RES_LEFT && centerXCoor <= HALF_RES_RIGHT)
-							talonDrive.reverseTankDrive(SLOWER_DRIVE_SPEED, SLOWER_DRIVE_SPEED);
-						else if (centerXCoor <= HALF_RES_LEFT)
-							talonDrive.reverseTankDrive(SWERVE_DRIVE_SPEED, STOP_SPEED);
-						else if (centerXCoor >= HALF_RES_RIGHT)
-							talonDrive.reverseTankDrive(STOP_SPEED, SWERVE_DRIVE_SPEED);	
-						else if (centerY.length < 2 && centerX.length < 2)
-							talonDrive.reverseTankDrive(SLOWER_DRIVE_SPEED, SLOWER_DRIVE_SPEED);
-					}
-					else
-					{
-							autonomousCase = STOP_WHEN_CLOSE_ENOUGH;
-							SmartDashboard.putString("wtf: ", "false");
-					}
-					break;
-					
-				case STOP_WHEN_CLOSE_ENOUGH:
-					if (sonicSensor.getDistance() > MIN_DISTANCE && sonicSensor.getDistance() < MAX_DISTANCE)
-						talonDrive.tankDrive(STOP_SPEED, STOP_SPEED);
-					break;
+
+			switch (autonomousCase) {
+			case C.Auto.DRIVE_STRAIGHT:
+				if (timer.get() < 1.3)
+					talonDrive.reverseTankDrive(C.Speed.STRAIGHT_DRIVE_SPEED, C.Speed.STRAIGHT_DRIVE_SPEED);
+				else
+					autonomousCase = C.Auto.MOMENTUM_STOP;
+				break;
+
+			case C.Auto.MOMENTUM_STOP:
+				if (timer.get() < 4)
+					talonDrive.tankDrive(C.Speed.STOP_SPEED, C.Speed.STOP_SPEED);
+				else
+					autonomousCase = C.Auto.TURN;
+				break;
+
+			case C.Auto.TURN:
+				talonDrive.tankDrive(-C.Speed.TURN_SPEED, C.Speed.TURN_SPEED);
+
+				if (/*
+					 * talonDrive.getAngle() < 65 && talonDrive.getAngle() > 55
+					 * &&
+					 */ centerY.length == 2 && centerX.length == 2) {
+					autonomousCase = C.Auto.STOP_WHEN_CENTERED;
+				}
+				break;
+
+			case C.Auto.STOP_WHEN_CENTERED:
+				centerXCoor = (centerX[0] + centerX[1]) / 2;
+				if (centerXCoor >= C.Cam.HALF_RES_LEFT && centerXCoor <= C.Cam.HALF_RES_RIGHT) {
+					talonDrive.tankDrive(C.Speed.STOP_SPEED, C.Speed.STOP_SPEED);
+					autonomousCase = C.Auto.SELF_CORRECT_DRIVE;
+				}
+				break;
+
+			case C.Auto.SELF_CORRECT_DRIVE:
+				if (centerY.length == 2 && centerX.length == 2)
+					centerXCoor = (centerX[0] + centerX[1]) / 2;
+				SmartDashboard.putString("reached: ", "true");
+				if (!(sonicSensor.getDistance() > C.Dist.MIN_DISTANCE
+						&& sonicSensor.getDistance() < C.Dist.MAX_DISTANCE)) {
+					if (centerXCoor >= C.Cam.HALF_RES_LEFT && centerXCoor <= C.Cam.HALF_RES_RIGHT)
+						talonDrive.reverseTankDrive(C.Speed.SLOWER_DRIVE_SPEED, C.Speed.SLOWER_DRIVE_SPEED);
+					else if (centerXCoor <= C.Cam.HALF_RES_LEFT)
+						talonDrive.reverseTankDrive(C.Speed.SWERVE_DRIVE_SPEED, C.Speed.STOP_SPEED);
+					else if (centerXCoor >= C.Cam.HALF_RES_RIGHT)
+						talonDrive.reverseTankDrive(C.Speed.STOP_SPEED, C.Speed.SWERVE_DRIVE_SPEED);
+					else if (centerY.length < 2 && centerX.length < 2)
+						talonDrive.reverseTankDrive(C.Speed.SLOWER_DRIVE_SPEED, C.Speed.SLOWER_DRIVE_SPEED);
+				} else {
+					autonomousCase = C.Auto.STOP_WHEN_CLOSE_ENOUGH;
+					SmartDashboard.putString("wtf: ", "false");
+				}
+				break;
+
+			case C.Auto.STOP_WHEN_CLOSE_ENOUGH:
+				if (sonicSensor.getDistance() > C.Dist.MIN_DISTANCE && sonicSensor.getDistance() < C.Dist.MAX_DISTANCE)
+					talonDrive.tankDrive(C.Speed.STOP_SPEED, C.Speed.STOP_SPEED);
+				break;
 			}
 			SmartDashboard.putNumber("Ultrasonic reading: ", sonicSensor.getDistance());
-			SmartDashboard.putNumber("Ultrasonic raw reading; ", sonicSensor.getVoltage());	
-			
-		}	
+			SmartDashboard.putNumber("Ultrasonic raw reading; ", sonicSensor.getVoltage());
+
+		}
 	}
 
 	public void operatorControl() {
 		boolean stop = false;
+		boolean reverseOrNah = false; 
+		boolean conveyorOnOrNah = false;
+		
 		while (isOperatorControl() && isEnabled()) {
 			slowTrigger = pad.getLeftTriggerValue();
-
-			eStop1Pressed = pad.getBackButton() || padSupport.getBackButton();
-			eStop2Pressed = pad.getStartButton() || padSupport.getStartButton();
-
-			if (eStop1Pressed && eStop2Pressed)
+			//winch = pad.getRightTriggerValue();
+			eStop1Pressed = pad.getBackButton();
+			reverseDirectionPressed = pad.getAButton();
+			conveyorPressed = pad.getLeftBumper();
+			sprintLeft = pad.getRawButton(9);
+			sprintRight = pad.getRawButton(10);
+			
+			if (eStop1Pressed)
 				stop = true;
+			
+			if (reverseDirectionPressed && !reverseDirectionToggle)
+			{
+				reverseDirectionToggle = true;
+				reverseOrNah = !reverseOrNah; 	
+			}
+			if (!reverseDirectionPressed)
+				reverseDirectionToggle = false; 
+			
+			if (sprintLeft && sprintRight && reverseOrNah && !stop)
+				talonDrive.tankDrive(pad.getLeftAnalogY(),
+						pad.getRightAnalogY());
+			
+			if (sprintLeft && sprintRight && !reverseOrNah && !stop)
+				talonDrive.reverseTankDrive(pad.getLeftAnalogY(),
+						pad.getRightAnalogY());
+			
+			if (reverseOrNah && !stop)
+				talonDrive.tankDrive(pad.getLeftAnalogY() * 0.75,
+						pad.getRightAnalogY() * 0.75);
+			else if (!reverseOrNah && !stop)
+				talonDrive.reverseTankDrive(pad.getLeftAnalogY() * 0.75,
+						pad.getRightAnalogY() * 0.75);
+			
+			
+			/*
+			if (conveyorPressed && !conveyorToggle)
+			{
+				conveyorToggle = true;
+				reverseOrNah = !reverseOrNah; 	
+			}
+			if (!conveyorPressed)
+				conveyorToggle = false; 
 
-			if (!stop) 
-				talonDrive.tankDrive(pad.getLeftAnalogY()  * -scaleTrigger(slowTrigger),
-						pad.getRightAnalogY() * -scaleTrigger(slowTrigger));
+			if (conveyorOnOrNah)
+				conveyorMotor.set(speed);
+			else if (!conveyorOnOrNah)
+				conveyorMotor.set(C.Speed.STOP_SPEED);
 			
-			SmartDashboard.putNumber("Ultrasonic reading: ", sonicSensor.getDistance());
-			SmartDashboard.putNumber("Ultrasonic raw reading; ", sonicSensor.getVoltage());
+			winchMotor.set(winch);
+			*/
+
+
+	
 			
-			SmartDashboard.putNumber("Gyroscope value: ", talonDrive.getAngle());
 		}
 	}
 
@@ -210,18 +226,18 @@ public class Robot extends SampleRobot {
 		return Math.min(1.0, 1.0 - 0.9 * trigger);
 	}
 
-	public double predictDistance (double pixels)
-	{
+	public double predictDistance(double pixels) {
 		// y = -1.542693 + (2487.625 - -1.542693)/(1 + (x/1.294396)^0.9814597)
 		double num = 2487.625 + 1.542693;
-		double den = 1 + Math.pow((pixels/1.294396), 0.9814597);
-		return (num/den -1.542693);
+		double den = 1 + Math.pow((pixels / 1.294396), 0.9814597);
+		return (num / den - 1.542693);
 	}
-	
+
 	public void test() {
-		while (isTest() && isEnabled())
-		{
-			talonDrive.tankDrive(-TURN_SPEED, TURN_SPEED);
+		camera = new UsbCamera("camera2", 0);
+
+		while (isTest() && isEnabled()) {
+
 		}
 	}
 }
